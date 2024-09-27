deace5c6911d21cc7a17c77f11d82aa2e4cdf322
Joserme <jose.mendez@campusdual.com>
2024-09-27 16:20:22 +0200

    Ejercicio17

--- src/main/java/com/campusdual/classroom/Exercise17.java
+++ src/main/java/com/campusdual/classroom/Exercise17.java
@@ -1,15 +1,38 @@
 package com.campusdual.classroom;
 
 public class Exercise17 {
-	public static int[] createEmptyIntArray(int positions) {}
+	public static int[] createEmptyIntArray(int positions) {
+		int[] array = new int[positions];
+		return array;
+	}
 
-	public static String[] createInitializedStringArray() {}
+	public static String[] createInitializedStringArray() {
+		String[] array2 = new String[3];
+		int n = 1;
+		int n1 = 2;
+		int n2 = 3;
+		array2[0] = String.valueOf(n);
+		array2[1] = String.valueOf(n1);
+		array2[2] = String.valueOf(n2);
 
-	public static void populateArray(int[] array, int index, int value) {}
+		return array2;
+	}
 
-	public static int arrayLength(int[] array) {}
+	public static void populateArray(int[] array, int index, int value) {
+		for (int i = 0; i < array.length; i++) {
+			array[index] = value;
+		}
+	}
 
-	public static void showValue(int[] array, int index) {}
+	public static int arrayLength(int[] array) {
+		int longitud = array.length;
+		return longitud;
+	}
+
+	public static void showValue(int[] array, int index) {
+		int valor = array[index];
+		System.out.println(valor);
+	}
 
 	public static void main(String[] args) {
 		int[] intArray = createEmptyIntArray(3);
